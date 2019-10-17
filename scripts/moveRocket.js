aa91d5448b1c2098cb4c992911cf3198e3803bb4
PatrycjaDom <patcho2808@gmail.com>
2019-10-17 10:08:14 +0200

    Disable scrolling down with space

--- scripts/moveRocket.js
+++ scripts/moveRocket.js
@@ -1,24 +1,35 @@
 $(document).ready(function()
 {
+    $('body').keydown(function(e)
+    {
+        if(e.which == 32)
+        {
+            e.preventDefault();
+        }
+    });
+
     $('body').keyup(function(e)
     {
         if(e.which == 32) // pressed space
         {
+            e.preventDefault();
             $('#rocket')
-                .animate({top: -250, left: -100},1000)
+                .animate({top: 0, left: '30%'},{ duration : 500, queue: false})
                 .animate(
-                { deg: 60 },
-                {
-                    duration: 1200,
-                    step: function(now) {
-                        now = now - 40;
-                        $(this).css({ transform: 'rotate(' + now  + 'deg)' });
+                    { deg: 60 },
+                    {
+                        duration: 300,
+                        step: function(now) {
+                            now = now - 40;
+                            $(this).css({ transform: 'rotate(' + now  + 'deg)' });
+                        },
+                        queue : false
                     }
-                }
             );
 
 
         }
+
 
     });
 });
